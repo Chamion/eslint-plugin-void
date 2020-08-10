ed18a432bad38685c17b11e10631538e7f4f0ae8
Jemi Salo <jemi.salo@gmail.com>
2020-08-10 12:12:31 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,269 @@
 # eslint-plugin-void
-Eslint plugin to lint use of the void operator
+
+This plugin enables the rules `side-effect` and `arrow-body` that can be used together or separately to enforce a style of using the void-operator.
+
+The rules are entirely --fixable so you can enforce them at any time with little effort even in a large codebase.
+
+## Usage
+
+Install package.
+
+```
+npm install eslint-plugin-void --save-dev
+```
+
+In `.eslintrc`
+
+```
+"plugins": [
+  "void"
+],
+"rules": {
+  "void/side-effect": {severity},
+  "void/arrow-body": {severity}
+}
+```
+
+Where severity is "error", "warn" or "off". You can omit "off" values.
+
+<details>
+<summary>side-effect</summary>
+## side-effect
+
+The void operator can be abused to write expressions that are nothing more than `undefined` with extra steps.
+
+```
+const isUndefined = value => value === void 0;
+```
+
+This rule disallows use of void without a possible side effect.
+
+## --fix
+
+Code can be automatically fixed with `--fix`. The above example would be fixed to
+
+```
+const isUndefined = value => value === undefined;
+```
+
+If your code editor supports eslint suggestions, this rule will allow you to apply the fix through a suggestion.
+
+## Default
+
+### Examples of correct code for this rule
+
+```
+void functionCall();
+
+void (object.field = value);
+```
+
+### Examples of incorrect code for this rule
+
+```
+void 0;
+
+void (() => functionCall());
+```
+
+## allowTraps
+
+The rule can be configured to allow arguments that may potentially trigger traps.
+
+To allow all traps configure `"allowTraps": true`.
+
+```
+rules: {
+  "void/side-effect": [{severity}, { "allowTraps": true }]
+}
+```
+
+To allow select traps configure `"allowTraps": {object}`.
+
+```
+rules: {
+  "void/side-effect": [
+    {severity},
+    {
+      "allowTraps": {
+        "get": {boolean},
+        "has": {boolean},
+        "ownKeys": {boolean}
+      }
+    }
+  ]
+}
+```
+
+You can omit false values from the configuration object.
+
+### Examples of correct code for this option
+
+```
+void object.field; // get
+
+void 'field' in object; // has
+
+void { ...object }; // ownKeys
+```
+</details>
+
+<details>
+<summary>arrow-body</summary>
+## arrow-body
+
+The void operator can be used to stop arrow functions leaking return values.
+
+```
+const mutationCallback = value => void (object.field = value);
+```
+
+The same effect can be achieved with a function body.
+
+```
+const mutationCallback = value => { object.field = value; };
+```
+
+This rule enforces one or the other style.
+
+## --fix
+
+Code can be automatically fixed with `--fix`. The above examples would be fixed to
+
+```
+const mutationCallback = value => {object.field = value;};
+```
+
+and
+
+```
+const mutationCallback = value => void (object.field = value);
+```
+
+respectively.
+
+If your code editor supports eslint suggestions, this rule will allow you to apply the fixes through a suggestion.
+
+## Default
+
+By default this rule prefers void in all cases.
+
+### Examples of correct code for this rule
+
+```
+(value => void sideEffect(value));
+
+(value => void (object.field = value));
+
+(value => void (sideEffect(value), object.field = value));
+
+(value => {
+  const input = value.trim();
+  sideEffect(value);
+});
+
+(value => sideEffect(value));
+```
+
+### Examples of incorrect code for this rule
+
+```
+(value => { sideEffect(value); });
+
+(value => { object.field = value; });
+
+(value => {
+  sideEffect(value);
+  object.field = value;
+});
+```
+
+## multi
+
+The `multi` option allows you to define your preference for multiple side effects in an arrow function body.
+
+### Example of correct code for { "multi": "void" }
+
+```
+(value => void (sideEffect(value), object.field = value));
+```
+
+### Example of incorrect code for { "multi": "void" }
+
+```
+(value => {
+  sideEffect(value);
+  object.field = value;
+});
+```
+
+### Example of correct code for { "multi": "body" }
+
+```
+(value => {
+  sideEffect(value);
+  object.field = value;
+});
+```
+
+### Example of incorrect code for { "multi": "body" }
+
+```
+(value => void (sideEffect(value), object.field = value));
+```
+
+### { multi: false }
+
+Setting the `multi` option to false will disable this rule for multiple side effect expressions in an arrow function body.
+
+## single
+
+The `single` option allows you to define your preference for a single side effect in an arrow function body.
+
+### Example of correct code for { "single": "void" }
+
+```
+(value => void sideEffect(value));
+```
+
+### Example of incorrect code for { "single": "void" }
+
+```
+(value => {
+  sideEffect(value);
+});
+```
+
+### Example of correct code for { "single": "body" }
+
+```
+(value => {
+  sideEffect(value);
+});
+```
+
+### Example of incorrect code for { "single": "body" }
+
+```
+(value => void sideEffect(value));
+```
+
+### { single: false }
+
+Setting the `multi` option to false will disable this rule for a single side effect in an arrow function body.
+
+## Prefer body
+
+To configure this rule to always prefer function body over the void operator configure both `single` and `multi`.
+
+```
+rules: {
+  "void/arrow-body": [{severity}, { "single": "body", "multi": "body" }]
+}
+```
+</details>
+
+## When not to use this plugin
+
+If you never want to use the void operator, use [no-void](https://eslint.org/docs/rules/no-void) instead.
+Even though this plugin offers fixes that `no-void` does not, the rules conflict so use of both is discouraged.
---
+++ index.js
@@ -0,0 +1,228 @@
+const createSideEffect = (context) => {
+  let allowTraps = !!context.options[0] && context.options[0].allowTraps;
+   if (!allowTraps) {
+    allowTraps = {};
+  } else if (allowTraps === true) {
+    allowTraps = {
+      get: true,
+      has: true,
+      ownKeys: true
+    };
+  }
+  const hasSideEffect = node => {
+    switch (node.type) {
+      case 'UpdateExpression':
+      case 'AssignmentExpression':
+      case 'CallExpression':
+      case 'OptionalCallExpression':
+      case 'NewExpression':
+      case 'YieldExpression':
+      case 'AwaitExpression':
+        return true;
+      case 'MemberExpression':
+        if (allowTraps.get) return true;
+        return hasSideEffect(node.object);
+      case 'UnaryExpression':
+        if (node.operator === 'delete') return true;
+      case 'SpreadElement':
+        return hasSideEffect(node.argument);
+      case 'BinaryExpression':
+        if (allowTraps.has && node.operator === 'in') return true;
+      case 'LogicalExpression':
+        return hasSideEffect(node.left) || hasSideEffect(node.right);
+      case 'SequenceExpression':
+        return node.expressions.some(hasSideEffect);
+      case 'ArrayExpression':
+        return node.elements.some(hasSideEffect);
+      case 'ConditionalExpression':
+        return hasSideEffect(node.consequent) || hasSideEffect(node.alternative);
+      case 'ObjectExpression':
+        return node.properties.some(property => (
+          property.type === 'Property'
+          && (
+            hasSideEffect(property.value)
+            || hasSideEffect(property.key)
+          )
+        ) || (
+          property.type === 'ExperimentalSpreadProperty'
+          && (
+            allowTraps.ownKeys ||
+            hasSideEffect(property.argument)
+          )
+        ));
+      case 'TemplateLiteral':
+        return node.expressions.some(hasSideEffect);
+      case 'TaggedTemplateExpression':
+        return hasSideEffect(node.quasi);
+      default:
+        return false;
+    }
+  };
+  return {
+    UnaryExpression(node) {
+      if (node.operator !== 'void' || hasSideEffect(node.argument)) return;
+      const fix = fixer => fixer.replaceText(node, 'undefined');
+      context.report({
+        node,
+        message: 'Unexpected void operator on non-side-effect argument. Use undefined instead.',
+        fix,
+        suggest: [{
+          desc: "Replace with undefined.",
+          fix
+        }]
+      });
+    }
+  };
+};
+
+const createArrowBody = (context) => {
+  const SIMPLE_SIDE_EFFECT_EXPRESSIONS = [
+    'CallExpression',
+    'AssignmentExpression',
+    ''
+  ];
+  let single = (context.options[0] || {}).single;
+  if (single == null) single = 'void';
+  let multi = (context.options[0] || {}).multi;
+  if (multi == null) multi = 'void';
+  const lintVoid = (node, scope) => {
+    const { body: block } = node;
+    if (
+      block.type !== 'BlockStatement' ||
+      block.body.length === 0 ||
+      (scope === 'single' && block.body.length > 1) ||
+      (scope === 'multi' && block.body.length === 1) ||
+      !block.body.every(
+        expressionStatement.type === 'ExpressionStatement' &&
+        SIMPLE_SIDE_EFFECT_EXPRESSIONS.includes(expressionStatement.expression.type)
+      )
+    ) return;
+    const fix = fixer => {
+      const source = context.getSourceCode();
+      let expressionsReplacement = block.body.map(
+        ({ expression }) => source.getText(expression)
+      ).join(', ');
+      if (block.body.length > 1 || block.body[0].expression.type === 'AssignmentExpression') {
+        expressionsReplacement = `(${expressionsReplacement})`;
+      }
+      const replacement = `void ${expressionsReplacement}`;
+      return fixer.replaceText(block, replacement);
+    };
+    context.report({
+      node: block,
+      message: 'Unexpected function body. Use void instead.',
+      fix,
+      suggest: [{
+        desc: "Replace with void expression.",
+        fix
+      }]
+    });
+  };
+  const lintBody = (node, scope) => {
+    const { body: voidExpression } = node;
+    if (
+      voidExpression.type !== 'UnaryExpression' ||
+      voidExpression.operator !== 'void' ||
+      (scope === 'single' && voidExpression.argument.type === 'SequenceExpression') ||
+      (scope === 'multi' && voidExpression.argument.type !== 'SequenceExpression')
+    ) return;
+    const expressions = voidExpression.argument.type === 'SequenceExpression'
+      ? voidExpression.argument.expressions
+      : [voidExpression.argument];
+    if (
+      !expressions.every(
+        expression => SIMPLE_SIDE_EFFECT_EXPRESSIONS.includes(expression.type)
+      )
+    ) return;
+    const fix = fixer => {
+      const source = context.getSourceCode();
+      const expressionsReplacement = expressions.map(
+        (expression) => source.getText(expression)
+      ).join(';');
+      const replacement = `{${expressionsReplacement};}`;
+      return fixer.replaceText(voidExpression, replacement);
+    };
+    context.report({
+      node: voidExpression,
+      message: 'Unexpected void operator. Use function body instead.',
+      fix,
+      suggest: [{
+        desc: 'Replace with function body.',
+        fix
+      }]
+    });
+  }
+  return {
+    ArrowFunctionExpression: (node) => {
+      if (single === 'void') lintVoid(node, 'single')
+      else if (single === 'body') lintBody(node, 'single');
+      if (multi === 'void') lintVoid(node, 'multi')
+      else if (multi === 'body') lintBody(node, 'multi');
+    }
+  };
+};
+
+module.exports = {
+  rules: {
+    'side-effect': {
+      create: createSideEffect,
+      meta: {
+        type: 'suggestion',
+        docs: {
+          description: 'Disallow void on non-side-effect arguments',
+          suggestion: true
+        },
+        fixable: 'code',
+        schema: [{
+          type: 'object',
+          properties: {
+            allowTraps: {
+              type: ['boolean', 'object'],
+              properties: {
+                get: { type: 'boolean' },
+                has: { type: 'boolean' },
+                ownKeys: { type: 'boolean' }
+              },
+              additionalProperties: false
+            }
+          },
+          additionalProperties: false
+        }]
+      }
+    },
+    'arrow-body': {
+      create: createArrowBody,
+      meta: {
+        type: 'suggestion',
+        docs: {
+          description: 'Prefer void operator or function body to void implicit arrow function return values.',
+          suggestion: true
+        },
+        fixable: 'code',
+        schema: [{
+          type: 'object',
+          properties: {
+            single: {
+              enum: ['void', 'body', false]
+            },
+            multi: {
+              enum: ['void', 'body', false]
+            }
+          },
+          additionalProperties: false
+        }]
+      }
+    }
+  },
+  configs: {
+    recommended: {
+      plugins: [
+        'void'
+      ],
+      rules: {
+        'void/side-effect': 2,
+        'void/arrow-body': 2
+      }
+    }
+  }
+};
